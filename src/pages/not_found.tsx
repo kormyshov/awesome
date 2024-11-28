38e53a1e3ffa77e42baf0bfc7db4988bdbe5b6be
Mikhail Kormyshov <kormyshov@yandex.ru>
2024-11-28 23:41:58 +0300

    Delete unused imports

--- src/pages/not_found.tsx
+++ src/pages/not_found.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from '../features/header';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
 
 
 export default function NotFound() {
